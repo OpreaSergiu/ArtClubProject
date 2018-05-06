7f81053b8c19004de23c945166daeba3f426b862
firstbloodidp <sergiu_andrei96@yahoo.com>
2018-05-06 20:44:28 +0300

    Add Project

---
+++ ArtClub/ArtClub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArtClub.MvcApplication" Language="C#" %>
---
+++ ArtClub/ArtClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtClub.Startup))]
+namespace ArtClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
